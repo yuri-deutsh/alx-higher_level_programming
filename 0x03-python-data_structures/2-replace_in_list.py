903ffdd0738d6830f4124ed29a33911046d321ba
nabil <nabilmkadmi@gmail.com>
2024-01-08 14:04:44 -0800

    py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 3-print_reversed_list_integer.py
-def print_reversed_list_integer(my_list=[]):
-    """Print all integers of a list in reverse order."""
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+# 2-replace_in_list.py
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
