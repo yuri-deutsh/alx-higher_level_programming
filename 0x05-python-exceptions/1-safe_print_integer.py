dff0c1ce3b4fdaeb2bcd7e3798e4bac7e0b87c37
nabil <nabilmkadmi@gmail.com>
2024-01-22 16:15:43 -0800

    py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
-        print("Value is not an integer")
+    except BaseException:
         return False
-
-if __name__ == "__main__":
-    value = 10
-    safe_print_integer(value)
-
-    value = "hello"
-    safe_print_integer(value)
