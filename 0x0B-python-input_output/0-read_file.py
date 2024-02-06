4c9d43b4881c620c7b245f291d24bad058e788ba
nabil <nabilmkadmi@gmail.com>
2024-02-06 11:25:30 -0800

    py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
+"""Module containing the function read_file"""
 
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+
+def read_file(filename=""):
+    """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
