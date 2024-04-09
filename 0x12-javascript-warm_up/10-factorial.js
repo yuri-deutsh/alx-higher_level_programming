1dfba95d9e171ac9fb4780c9117cecef4827151f
nabil <nabilmkadmi@gmail.com>
2024-04-08 20:28:23 -0700

    java

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-// computes and prints a factorial
-
 function factorial (n) {
-  if ((isNaN(n)) || (n === 1)) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
